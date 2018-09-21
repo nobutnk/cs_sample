c5a6dfb8aaa30d1bac957aed3dc1f3c0f3a220aa
nobutnk <nobutnk@gmail.com>
2018-09-21 09:27:29 +0900

    branch設定方法見直し

---
+++ tfs/git/GitClientSample2.cs
@@ -0,0 +1,52 @@
+Class GitClientUtils {
+
+  /**
+   * repository取得するところまでは想像。
+   */
+  public File getFileFromGitRepository(string repositoryName, string url)
+  {
+    // このあたりは適当
+    Microsoft.VisualStudio.Services.Common.WindowsCredential credential
+     = new Microsoft.VisualStudio.Services.Common.WindowsCredential(new NetworkCredential(XXX));
+
+    // Credencial取得できる？
+    var vssCredentials = new VssCredentials(credential);
+
+    // GitHttpClient使えるの前提
+    // 対象ファイルのURLをここで渡す
+    GitHttpClient client = new GitHttpClient(new Uri(url), vssCredentials);
+
+    try {
+        // repositoryは取得できる前提
+        var repositories = await client.GetRepositoriesAsync();
+        var repository = repositories.FirstOrDefault(i => i.Name == repositoryName);
+        var repositoryId = repository.Id;
+        GitVersionDescriptor getDes = new GitVersionDescriptor() {Version = "your-branch-name"};
+
+        // フォルダ配下取得するときは、GetItemsAsyncで、scopePathをフォルダ単位で設定
+        var items = await client.GetItemsAsync(repositoryId, scopePath:"/Solution1/ConsoleApplication1", versionDescriptor:gitDes, recursionLevel:VersionControlRecursionType.Full);
+        foreach (var item in items)
+        {
+            if(!item.IsFolder)
+            {
+                var blob = await client.GetBlobContentAsync(repositoryId, item.ObjectId, ...);
+                // あとは省略。Streamからファイル化してください。
+            }
+        }
+
+        // ファイル単位のときはこんなかんじのはず.var blobはTask<Stream>のこと
+        var item = await client.GetItemAsync(repositoryId, path:"ファイルパス設定", versionDescriptor:gitDes);
+        var blob = await client.GetBlobContentAsync(repositoryId, item.ObjectId, ...);
+        // あとは省略。Streamからファイル化してください。
+
+        // 別手法。これで１行で取得できそう。引数はもっとある。
+        // var blobはTask<Stream>のこと
+        var blob = await client.GetItemContentAsync(repositoryId, path:"ファイルパス設定", versionDescriptor:gitDes);
+        // あとは省略。Streamからファイル化してください。
+    }
+    catch(Exception e)
+    {
+      // 例外処理
+    }
+  }
+}
